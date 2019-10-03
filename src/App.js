563459c4b7af84a77c0f7ea53229b5cf9218cff8
kaollie74 <kaollie74@gmail.com>
2019-10-03 17:35:01 -0500

    install tachyons and style robot cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './App.css';
 
-class App extends Component {
-
-  render () {
-    return (
-      <div className='f1 tc'>
-        <p>Hello</p>
+const App = () => {
+  return (
+    <>
+      <h1>RoboFriends</h1>
+      <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img alt='robots' src='https://robohash.org/test?200x200' />
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
       </div>
-    )
-  }
-
+      <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img alt='robots' src='https://robohash.org/test2?200x200' />
+        <h2>Bill Doe</h2>
+        <p>Bill.doe@gmail.com</p>
+      </div>
+      <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img alt='robots' src='https://robohash.org/test3?200x200' />
+        <h2>Sam Doe</h2>
+        <p>Sam.doe@gmail.com</p>
+      </div>
+    </>
+  )
 }
+
+
 
 
 export default App;
